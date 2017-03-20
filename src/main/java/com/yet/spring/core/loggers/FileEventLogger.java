5c827894b18c5a2197b90eafff8c341a457eca43
Vladimir Belyashov <k51201@vampa.ru>
2017-03-21 02:15:59 +0500

    Added FileEventLogger

---
+++ src/main/java/com/yet/spring/core/loggers/FileEventLogger.java
@@ -0,0 +1,37 @@
+package com.yet.spring.core.loggers;
+
+import com.yet.spring.core.beans.Event;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+/**
+ * Created by vampa on 21.03.2017.
+ */
+public class FileEventLogger implements EventLogger {
+    private String fileName;
+    private File file;
+
+    public FileEventLogger(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void init() throws IOException {
+        this.file = new File(fileName);
+        if (file.exists() && !file.canWrite()) {
+            throw new IllegalArgumentException("Can't write to file " + fileName);
+        } else if (!file.exists()) {
+            file.createNewFile();
+        }
+    }
+
+    public void logEvent(Event event) {
+        try {
+            FileUtils.writeStringToFile(file, event.toString() + "\n", Charset.defaultCharset(),true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
